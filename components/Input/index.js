14c39063b0c87ca7b8aad6cc87e172517600028b
krendeleno <krendeleno@gmail.com>
2021-12-13 02:23:15 +0800

    refactor: refactor input

--- components/Input/index.js
+++ components/Input/index.js
@@ -2,15 +2,13 @@ import React, {useState} from 'react';
 import {TextInput, Text, View} from 'react-native';
 
 
-const Input = ({ value, name, type, onChange, placeholder, error, errorMessage, ...props }) => {
+const Input = ({ name, onChange, error, errorMessage, ...props }) => {
 
     return (
         <View>
             <TextInput
                 {...props}
-                placeholder = {placeholder}
-                value={value}
-                onChangeText={text => onChange({ name, type, text })}
+                onChangeText={text => onChange({ name, text })}
             />
             {!error && <Text>{errorMessage}</Text>}
         </View>
