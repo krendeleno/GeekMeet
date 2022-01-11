018924822da6d15fa5e03e33fd752fe00157464c
GordeevaUI <gordeeva.uljana@yandex.ru>
2022-01-11 15:11:35 +0700

    feat: events, users

--- MockData/events.js
+++ MockData/events.js
@@ -1,5 +1,130 @@
 export const events = [{
     id:1,
+    title: "Настолочки",
+    adminId: 1,
+    dateTime: new Date(43270444234),
+    place:'Антикафе "Тоска"',
+    address:"пр. Разочарования, 13",
+    participants: 9,
+    tags:[3,10], 
+    isMarked: true,
+    requestStatus:"accepted",
+    description:'Ищем с другом компанию для игры в "карты конфликта" в ближайшие выходные. Собираемся в центре города в антикафе "Тоска". Скидываемся по 300 рублей на питсу из Додо, напитки и вход в антикафе. Рады всем))',
+    members:[2, 5, 7,8,9,10],
+    image:"https://sun9-85.userapi.com/impg/WWjM5X5GBkpVwF4kdPZcf0HcRL_U0oFzpRIv_Q/BVzqPVPMoas.jpg?size=225x225&quality=96&sign=450012cff23c434d629c5bbfe91f8b6d&type=album"
+},
+{
+    id:2,
+    title: "Приучаем себя к бегу",
+    adminId: 2,
+    dateTime:new Date(43293554234),
+    place:'парк "People Park"',
+    address:"ул. Лесная, 42",
+    participants: 3,
+    tags:[1,18], 
+    isMarked: false,
+    requestStatus:"sent",
+    description:"Решил начать бегать по утрам, пока два раза в неделю - Вторник и Четверг с около 10 до около 11. Одному как то уныло, поэтому приглашаю присоединиться ко мне. Можем общаться во время пробежки (если будут на это силы). Учтите - я новичок, опытным бегунам наверное будет неинтересно.",
+    members:[7, 10, 8, 9],
+    image:"https://uk.hoboetc.com/images/zdorove/kak-opredelit-anaerobnij-porog_4.jpg"
+},
+{
+    id:3,
+    title: "Рыцарский турнир",
+    adminId: 7,
+    dateTime: new Date(43243444234),
+    place:"Замок Фаркуада",
+    address:"ул. Сказочная, 7",
+    participants: 20,
+    tags:[6,18], 
+    isMarked: false,
+    requestStatus:"rejected",
+    description:"Мы с Ослом будем участвовать в рыцарском турнире в замке у лорда Фаркуада. Будет много выпивки и веселья. Присоединяйтесь к нам! Посмотрим, кто победит ;)",
+    members:[5, 4, 3, 2],
+    image:"https://pbs.twimg.com/media/CwyGXcHUQAAKBBn.jpg"
+},
+{
+    id:4,
+    title: "Поход в киношку",
+    adminId: 3,
+    dateTime: new Date(422222324),
+    place:'кино "StarRail"',
+    address:"ул. Пушкина, 666",
+    participants: 1,
+    tags:[31], 
+    isMarked: false,
+    requestStatus:"accepted",
+    description:"Ищу человека, с кем можно сходить в кино на Энканто, потому что мои друзья не любят мультфильмы. У меня там работает брат, поэтому будет скидка на попкорн",
+    members:[5, 6, 8, 2],
+    image:"https://bilook.ru/storage/images/16226522563.jpg"
+},
+{
+    id:5,
+    title: "Недопрятки",
+    adminId: 10,
+    dateTime: new Date(644324777),
+    place:'Хата',
+    address:"ул. Кирочная, 20",
+    participants: 10,
+    tags:[2,14], 
+    isMarked: true,
+    requestStatus:"accepted",
+    description:" Прячем картинки в квартире брата! Брат уехал в Шри-Ланку, оставил меня тут одного. Теперь нужно ответить ему за это, чтобы он надолго запомнил. Жду вас с предложениями, где лучше всего в доме спрятать валдо",
+    members:[5, 6, 8, 2],
+    image:"https://sun9-53.userapi.com/impg/l6PBP9wsIYo_mBG9XQ6j8KTts6YGUxNWcFekDQ/veL_zY0EY5I.jpg?size=541x658&quality=96&sign=d1f6daebe2b92137eeb7c8ac4e5594bc&type=album"
+},
+{
+    id:6,
+    title: "Поиск крестражей",
+    adminId: 4,
+    dateTime: new Date(4324),
+    place:'Хогвартс',
+    address:"ул. Тайн, 1",
+    participants: 3,
+    tags:[15, 30], 
+    isMarked: false,
+    requestStatus:"rejected",
+    description:"Собираю команду, чтобы отправиться в грандиозное путешествие с целью уничтожить все крестражи Волан-де-Морта, а потом умереть. Предупреждаю, будет жестко, напишите завещание. Магглы мимо.",
+    members:[5, 6, 8, 2],
+    image:"https://sun9-37.userapi.com/impg/5v6O-0gitNTG62XXywpX-CoRh0EsDoLfHDDUcQ/7BAjFzhepFI.jpg?size=640x448&quality=96&sign=8fb08f37fad69d4f0eabe0af0d91045e&type=album"
+},
+{
+    id:7,
+    title: "Культурная тусовочка",
+    adminId: 5,
+    dateTime: new Date(4324),
+    place:'Домик',
+    address:"ул. Пулковская, 6",
+    participants: 3,
+    tags:[30, 33], 
+    isMarked: true,
+    requestStatus:"default",
+    description:"Собираем порядочных людей 18-23 лет на тусовку в квартире. Будет выпивка, настолки, музыка и приятное общение. Еду и выпивку каждый покупает сам, также можно заказать доставку.",
+    members:[4, 6, 8, 2],
+    image:"https://sun9-65.userapi.com/impg/GLYXq3z6f2KUXcL-4ki1nAggdhV2e93CQf1L7Q/xHrUpXTc8ZI.jpg?size=478x478&quality=96&sign=52322f258e4bf14c92de1eb9cbde810e&type=album"
+},
+{
+    id:8,
+    title: "Homework",
+    adminId: 4,
+    dateTime: new Date(4324),
+    place:'Хогвартс',
+    address:"ул. Тайн, 1",
+    participants: 3,
+    tags:[37], 
+    isMarked: false,
+    requestStatus:"accepted",
+    description:"Скоро экзамены, а я ничего не делал весь год, потому что сражался со злом, и не знаю у кого списать.С меня шоколадная лягушка и бесконечная благодарность",
+    members:[2],
+    image:"https://sun9-84.userapi.com/impg/PD5J00MQ5n0M7obvpKHvQ1M9F9MCamJDYFg_tw/NohnYI_baKQ.jpg?size=1800x1200&quality=96&sign=347bc480648628e0a739b436e48d25a3&type=album"
+},
+]
+
+
+
+
+/* export const events = [{
+    id:1,
     title: "First event",
     adminId: 1,
     dateTime: new Date(4324234),
@@ -58,4 +183,4 @@ export const events = [{
     members:[5, 6, 8, 2],
     image:"https://phonoteka.org/uploads/posts/2021-06/1623424979_22-phonoteka_org-p-tekstura-vecherinka-krasivo-23.jpg"
 },
-]
+] */
